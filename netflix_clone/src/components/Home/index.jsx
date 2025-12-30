2501abbbe44897fb919fe78d65c43845629f64ee
SrithinRaveendran <srithinraveendran@gmail.com>
2025-12-30 20:50:34 +0530

    node update categorised the home page

--- netflix_clone/src/components/Home/index.jsx
+++ netflix_clone/src/components/Home/index.jsx
@@ -1,7 +1,7 @@
 import './index.css'
 import { MyContext } from '../context'
 import { useContext } from 'react'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
@@ -12,43 +12,87 @@ import Carousel from '../carousel';
 
 import { fetchTrendingMovies } from '../tmdb';
 
-// tmdbGenres.js
 
- const tmdb_genre_list = [
-    { id: 28, name: "Action" },
-    { id: 12, name: "Adventure" },
-    { id: 16, name: "Animation" },
-    { id: 35, name: "Comedy" },
-    { id: 80, name: "Crime" },
-    { id: 99, name: "Documentary" },
-    { id: 18, name: "Drama" },
-    { id: 10751, name: "Family" },
-    { id: 14, name: "Fantasy" },
-    { id: 36, name: "History" },
-    { id: 27, name: "Horror" },
-    { id: 10402, name: "Music" },
-    { id: 9648, name: "Mystery" },
-    { id: 10749, name: "Romance" },
-    { id: 878, name: "Science Fiction" },
-    { id: 10770, name: "TV Movie" },
-    { id: 53, name: "Thriller" },
-    { id: 10752, name: "War" },
-    { id: 37, name: "Western" },
-];
+
+// tmdbGenres.js the below list is comment its importsant for reference don't remove it
+// const tmdb_genre_list = [
+//     { id: 28, name: "Action" },
+//     { id: 12, name: "Adventure" },
+//     { id: 16, name: "Animation" },
+//     { id: 35, name: "Comedy" },
+//     { id: 80, name: "Crime" },
+//     { id: 99, name: "Documentary" },
+//     { id: 18, name: "Drama" },
+//     { id: 10751, name: "Family" },
+//     { id: 14, name: "Fantasy" },
+//     { id: 36, name: "History" },
+//     { id: 27, name: "Horror" },
+//     { id: 10402, name: "Music" },
+//     { id: 9648, name: "Mystery" },
+//     { id: 10749, name: "Romance" },
+//     { id: 878, name: "Science Fiction" },
+//     { id: 10770, name: "TV Movie" },
+//     { id: 53, name: "Thriller" },
+//     { id: 10752, name: "War" },
+//     { id: 37, name: "Western" },
+//];
+
+
+
+
+
 
 const Home = () => {
     const { name, movieData, setMovieData } = useContext(MyContext)
     console.log(name)
 
+
+    const [StateAdventureList, setAdventureList] = useState([])
+    const [StateRomanceList, setRomanceList] = useState([])
+
     useEffect(() => {
 
-        async function getmovie(){
+        async function getmovie() {
             const data = await fetchTrendingMovies()
             setMovieData(data)
+            // console.log(RomanceList,"rom")
         }
         getmovie()
 
     }, [])
+
+    useEffect(() => {
+        //(i feel this is dump idea to use here need to find the another good way)
+        // here  i am mapping the each items from the apicall to get adventure movies 
+        // only based on the list above which i got from api document
+        // so that i can avoid a api to get the genre code details.. later i will make it automate using object
+
+        
+        const RomanceList = []
+        const adventurefunction = () => {
+            const AdventureList = []
+            movieData && movieData.map(each => {
+                if (each.genre_ids.includes(12)) {
+                    AdventureList.push(each)
+                }
+                
+            })
+            //remember you canot directly update the state from 
+            // useEffect hook its dould be inside the funtion
+            console.log('check render')
+            setAdventureList(AdventureList)
+        }
+        adventurefunction()
+        // same process to get the romantic movie
+
+        movieData && movieData.map(each => {
+            if (each.genre_ids.includes(10749) | (each.genre_ids.includes(18))) {
+                RomanceList.push(each)
+            }
+            setRomanceList(RomanceList)
+        })
+
+    }, [movieData])
 
     return (
         <div className='background-home'>
@@ -76,14 +120,14 @@ const Home = () => {
                     <CategoryButton name="Movie" />
                 </div>
 
-         
+
                 <>
                     <h2 className="categoryHeading"> Movie</h2>
-                        <Carousel movieData = {movieData}/>
-                        <h2 className="categoryHeading"> Movie</h2>
-                        <Carousel movieData = {movieData}/>
-                        <h2 className="categoryHeading"> Movie</h2>
-                        <Carousel movieData = {movieData}/>
+                    <Carousel movieData={movieData} />
+                    <h2 className="categoryHeading">Adventure Hits</h2>
+                    <Carousel movieData={StateAdventureList} />
+                    <h2 className="categoryHeading">Romantic And Drama Hits</h2>
+                    <Carousel movieData={StateRomanceList} />
                 </>
 
             </div>
--- netflix_clone/src/components/tmdb/index.js
+++ netflix_clone/src/components/tmdb/index.js
@@ -1,5 +1,6 @@
 const API_TOKEN = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMDlkYmM3Yjc4YmJhZDZjNjI4NWI3MTI4MTU4MWRhMiIsIm5iZiI6MTc2Njg0MDc2My42NDQsInN1YiI6IjY5NGZkOWJiYzM4YzMxYTJjZjdhYzA5NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.PYuY3VdjKvm4JpYHm2PaIMb-MxI3-HnrvPdsBAElX1o'; // Use the Read Access Token here
 const BASE_URL = 'https://api.themoviedb.org/3';
+
 export async function fetchTrendingMovies() {
     try {
         const response = await fetch(`${BASE_URL}/trending/movie/day`, {
@@ -16,6 +17,9 @@ export async function fetchTrendingMovies() {
 
         const data = await response.json();
         console.log(data.results);
+        
+
+
         return (data.results)// This will show the list of trending movies
     } catch (error) {
         console.error('Fetch error:', error);
